01a0da03abbfeb640b3b591de3fb8d0b68deadb3
RamoZack <54585072+RamoZack@users.noreply.github.com>
2026-02-07 16:50:31 -0500

    Create outputs.tf
    lol

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "name" {
+  description = "Applicant name"
+  value       = "Omar Zakaria"
+}
+
+output "position_title" {
+  description = "Title of the position being applied for"
+  value       = "Senior DevOps Engineer"
+}
