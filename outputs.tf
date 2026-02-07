c690ffebc467d630662a552d0d856af0880bba2d
RamoZack <54585072+RamoZack@users.noreply.github.com>
2026-02-07 16:55:52 -0500

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "name" {
-  description = "Applicant name"
-  value       = "Omar Zakaria"
-}
-
-output "position_title" {
-  description = "Title of the position being applied for"
-  value       = "Senior DevOps Engineer"
-}
